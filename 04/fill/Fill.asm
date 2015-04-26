dc6794a16611e7209df7a59594e0794419526364
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-04-27 00:48:44 +0900

    Finished project04.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,9 +3,50 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+(MAIN_LOOP)
+    @i
+    M=0
+    @KBD
+    D=M
+    @FILL_BLACK
+    D;JNE
+    @FILL_WHITE
+    0;JMP
+
+(FILL_BLACK)
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    M=M+1
+    D=M
+    @8192
+    D=A-D
+    @FILL_BLACK
+    D;JGT
+    @MAIN_LOOP
+    0;JMP
+
+(FILL_WHITE)
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    M=M+1
+    D=M
+    @8192
+    D=A-D
+    @FILL_WHITE
+    D;JGT
+    @MAIN_LOOP
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @i
+    M=0
+    @R2
+    M=0
+
+(LOOP)
+    @R1
+    D=M
+    @i
+    D=D-M
+    @END
+    D;JLE
+
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
