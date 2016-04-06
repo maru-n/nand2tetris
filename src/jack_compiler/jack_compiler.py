ad06b34274df836225538845c40d08f04deca303
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2016-04-07 07:45:56 +0900

    change file names of jack analyzer for new chapter.

---
+++ src/jack_compiler/jack_compiler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import glob
+from os import path
+from compilation_engine import CompilationEngine
+
+
+def analyze(src_jack_file, output):
+    print(src_jack_file)
+    compilation_engine = CompilationEngine(src_jack_file, output)
+    compilation_engine.compile()
+
+def compile(src_jack_file, output):
+    print(src_jack_file)
+    compilation_engine = CompilationEngine(src_jack_file, output)
+    compilation_engine.compile()
+
+if __name__ == '__main__':
+
+    input = sys.argv[1]
+
+    if path.isfile(input):
+        src_jack_files = [input]
+    else:
+        src_jack_files = glob.glob(path.join(input, '*.jack'))
+
+    for src_jack_file in src_jack_files:
+        #output = sys.stdout
+        if len(sys.argv) == 3 and sys.argv[2] == '-a':
+            output = open(path.splitext(src_jack_file)[0] + ".xml", 'w')
+            analyze(src_jack_file, output)
+        else:
+            output = open(path.splitext(src_jack_file)[0] + ".vm", 'w')
+            compile(src_jack_file, output)
